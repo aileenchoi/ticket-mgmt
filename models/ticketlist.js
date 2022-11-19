c612d43f2f2a1dcbe2180b888d9d6dd3abedbbcd
Aileen Choi <ac.aileenchoi@gmail.com>
2022-11-19 13:50:23 -0500

    Added route, model, controllers for home page

---
+++ models/ticketlist.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ticketListSchema = new mongoose.Schema({
+  subject: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  severity: {
+    type: String,
+    required: true,
+  },
+  assignedTo: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Open"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+//MongoDB Collection named here - will give lowercase plural of name 
+module.exports = mongoose.model("TicketList", ticketListSchema, 'tickets');
---
+++ server.js
@@ -0,0 +1,21 @@
+//Declare Variables
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const mongoose = require("mongoose");
+const connectDB = require("./config/database");
+const homeRoutes = require("./routes/home")
+require('dotenv').config({path: './config/.env'})
+
+connectDB()
+
+//Set Middleware
+app.set("view engine", "ejs");
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+
+//Set Routes
+app.use('/', homeRoutes)
+
+//Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
